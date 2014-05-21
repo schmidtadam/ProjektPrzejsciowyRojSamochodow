43ef3cc3a3029773fdff1e8b3d459b7be7f199b7
NBlastMax <nblastmax@gmail.com>
2014-05-21 19:54:44 +0200

    - Czyszczenie kodu, usunięcie niepotrzebnych zmiennych - Poszerzono koła samochodu i zwiększono tarcie, by polepszyć trakcje - Usunięto nastawianie momentu obrotowego na koła. Teraz ustawiana jest stałą prędkość kątową kół - samochody poruszają się ze stałą prędkością i jest mniejsza szansa na wylecenie samochodu z trasy - Platforma jest teraz geometrią. Auta nie wpływają już na jej pozycję - Samochody generują się teraz z nieco mniejszymi kołami - nie powodują już błędów - Zmiana wartości jednej zmiennej Ilość_Aut skaluje resztę programu - pozwala na kontrolę liczby aut, redukuje bądź zmniejsza szerokość platformy w razie potrzeby - Przygotowanie do losowania kolorów aut - jeszcze nie działa - Usunięcie niekoniecznych komentarzy

--- Test Terenowy.cpp
+++ Test Terenowy.cpp
@@ -1,8 +1,12 @@
-﻿#include <ode/ode.h>
+#include <ode/ode.h>
 #include <drawstuff/drawstuff.h>
 #include <time.h> 
 #include <conio.h>
 #include <cmath>
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
 
 #ifdef _MSC_VER
 #pragma warning(disable:4244 4305)
@@ -11,6 +15,7 @@
 #ifdef dDOUBLE
 #define dsDrawBox      dsDrawBoxD
 #define dsDrawSphere dsDrawSphereD
+#define dsDrawCylinder dsDrawCylinderD
 #endif
 
 #define DENSITY (5.0)	
@@ -20,42 +25,85 @@ static dSpaceID space;
 static dGeomID  ground;
 static dJointGroupID contactgroup;
 
-
-
 struct MyObject
 {
     dBodyID body;
 	dGeomID geom;
 };
 
+class Car
 
-const int DL_Most = 40;
+{
+	public:
+
+	MyObject RamaP[2];
+	MyObject Rama[4];
+	MyObject Opona[4];
+	void MakeRama(int licznik);
+	void MakeCar(int licznik);
+
+	dJointID joint[5];
+	dJointID joint2[4];
+	dJointID joint3[2];
+
+	dMatrix3 R4,R5;
+	dReal radius;
+	dReal length;
+	dReal wysokosc;
+	dReal os;
+	dReal WymiaryPoprz[3]; 
+	dReal WymiaryRama[3];
+	dReal czerwony;
+	dReal zielony;
+	dReal niebieski;
+	Car();
+};
+
+Car::Car()
+{
+	const dReal m = 0.1; //masa ko�a
+	const dReal z = 0.1; //masa czesci kabiny
+	const dReal z2 = 0.1; //masa czesci kabiny
+	const dReal z3 = 0.1; //masa czesci kabiny
+	const dReal z4 = 0.1; //masa czesci kabiny
+	const dReal z5 = 0.1; //masa czesci kabiny
+	const dReal z6 = 0.1; //masa czesci kabiny
+	const dReal z7 = 0.1; //masa czesci kabiny
+	const dReal z8 = 0.1; //masa czesci kabiny
+}
+
+dReal przesuniecie=2;
+
+const int Ilosc_Aut = 4;
+const int DL_Most = 30;
 const int Test_Pilek = 5;
 
+//Obiekty
+Car samochod[Ilosc_Aut];
+dGeomID platforma[2];
 dGeomID box[DL_Most];
-MyObject platforma[2];
 MyObject ball[Test_Pilek];
 
+//Kule
 const dReal r = 0.35;
-const dReal m = 5;
-const dReal mp = 400.0;
 const dReal m1 = 1.0;
-const dReal *pos1,*R1,*pos2,*R2;
+const dReal p = 0.05;
 
-dReal sides[3] = {5,0.4,0.05};
-dReal platforma_sides[3] = {5,3,2};
-dReal platforma_sides2[3] = {5,3,0};
-// boki platform
+//Macierz obrotu
+dMatrix3 R; 
+dReal pi=3.14;
 
-dMatrix3 R; // macierz obrotu
+//Platformy i belki
+dReal sides[3] = {Ilosc_Aut*2.2,0.4,0.05};
+dReal platforma_sides[3] = {Ilosc_Aut*2.2,3,2};
+dReal platforma_sides2[3] = {Ilosc_Aut*2.2,3,0};
 
-dReal angle[DL_Most]; // katy belek
+dReal angle[DL_Most];
 dReal angle_deg[DL_Most];
 dReal wys[DL_Most];
-dReal odl[DL_Most]; // odchyłki belek
-dReal odlp[2]; // pozycje platform
+dReal odl[DL_Most];
+dReal odlp[2]; 
 dReal wysp[2];
-
 
 static void nearCallback(void *data, dGeomID o1, dGeomID o2)
 {
@@ -69,37 +117,32 @@ static void nearCallback(void *data, dGeomID o1, dGeomID o2)
 	  for (int i=0; i < n; i++) 
 	  {
 		contact[i].surface.mode = dContactBounce;
-		contact[i].surface.mu   = dInfinity;
-		contact[i].surface.bounce     = 0.2; // (0.0~1.0) restitution parameter
+		contact[i].surface.mu   = 0.6;
+		contact[i].surface.bounce     = 0.0; // (0.0~1.0) restitution parameter
 		contact[i].surface.bounce_vel = 0.0; // minimum incoming velocity for bounce
 		dJointID c = dJointCreateContact(world,contactgroup,&contact[i]);
 		dJointAttach (c,dGeomGetBody(contact[i].geom.g1),dGeomGetBody(contact[i].geom.g2));
 	}
-  
- 
+   
 }
-
 
 static void start()
 {
-    static float xyz[3] = {12,3,2};    
-    static float hpr[3] = {-180, 0, 0}; 
+    static float xyz[3] = {12,1.8,5.5};    
+    static float hpr[3] = {-190, -22,0}; 
     dsSetViewpoint (xyz,hpr);           
 }
 
-
 static void simLoop (int pause)
 {
-    const dReal *pos1,*R1,*pos2,*R2, *pos3,*R3;
+    const dReal *pos1,*R1,*pos2,*R2, *pos3,*R3,*pos4,*R4,*pos6,*R6,*pos7,*R7;
 	
+	dSpaceCollide(space,0,&nearCallback);
 	
-	dSpaceCollide(space,0,&nearCallback); //KOLIZJE WYŁĄCZONE -> CRASH PRZY GENEROWANIU BELEK. PRZY GENEROWANIU SAMYCH PLATFORM PROGRAM DZIAŁA.
 	dWorldStep(world,0.01);
 	dJointGroupEmpty(contactgroup);
-	
-    dWorldStep(world,0.01);
-	
-	for (int j=1; j<(DL_Most+1); j++) // belki
+
+	for (int j=1; j<(DL_Most+1); j++) //Belki
 	{
 		dRFromAxisAndAngle(R, 1, 0, 0, angle_deg[j-1]);
 		dsSetColorAlpha (0.9,0.6,0,2);
@@ -111,16 +154,50 @@ static void simLoop (int pause)
 		dsDrawBox(pos1,R1,sides);
 	}
 
-	for (int k=1; k<3; k++) // platformy
+	for (int k=1; k<3; k++) //Platformy
 	{
 		dsSetColorAlpha (0.5,0.5,0.5,1);
-		pos2 = dBodyGetPosition(platforma[k-1].body);
-		R2   = dBodyGetRotation(platforma[k-1].body);
+		dGeomSetPosition(platforma[k-1],0,odlp[k-1],wysp[k-1]);
+		pos2 = dGeomGetPosition(platforma[k-1]);
+		R2   = dGeomGetRotation(platforma[k-1]);
 		if (k==1) {	dsDrawBox(pos2,R2,platforma_sides);}
 		else dsDrawBox(pos2,R2,platforma_sides2);
 	}
 
-	for (int p=0; p<Test_Pilek; p++) // piłki
+	for(int l=0;l<Ilosc_Aut;l++)//Auta
+	{
+		for (int i=0; i<4 ; i++) //Opona
+		{
+    		dsSetColorAlpha (1,1,1,1);
+			pos6 = dBodyGetPosition(samochod[l].Opona[i].body);
+			R6 = dBodyGetRotation(samochod[l].Opona[i].body);
+			dsDrawCylinder(pos6,R6,samochod[l].length,samochod[l].radius);
+		}
+
+		for (int i=0; i<1 ; i++) //Auto
+		{
+			dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
+			pos4 = dBodyGetPosition(samochod[l].RamaP[i].body);
+			R4   = dBodyGetRotation(samochod[l].RamaP[i].body);
+			dsDrawBox(pos4,R4,samochod[l].WymiaryPoprz);
+	    }
+		
+		for (int i=0; i<4 ; i++) //Auto
+		{
+			dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
+			pos7 = dBodyGetPosition(samochod[l].Rama[i].body);
+			R7   = dBodyGetRotation(samochod[l].Rama[i].body);
+			dsDrawBox(pos7,R7,samochod[l].WymiaryRama);
+		}
+
+	dBodySetAngularVel(samochod[l].Opona[0].body, -3, 0, 0);
+	dBodySetAngularVel(samochod[l].Opona[1].body, -3, 0, 0); 
+	dBodySetAngularVel(samochod[l].Opona[2].body, -3, 0, 0);
+	dBodySetAngularVel(samochod[l].Opona[3].body, -3, 0, 0);
+}
+
+	/* KULE DO TESTU FIZYKI - NIEAKTYWNE
+	for (int p=0; p<Test_Pilek; p++) // Kule
 	{
 	    dsSetColor(0.0,0.5,0.0);
 		dsSetSphereQuality(3);
@@ -128,143 +205,68 @@ static void simLoop (int pause)
 		R3   = dBodyGetRotation(ball[p].body);
 		dsDrawSphere(pos3,R3,r);
 	}
-
-
-	/* ***STARA WERSJA SIMLOOP***
-
-	for (int k=1; k<3 ; k++)
-		{
-		dsSetColorAlpha (0.5,0.5,0.5,1);
-		pos2 = dBodyGetPosition(platforma[k-1].body);
-		R2   = dBodyGetRotation(platforma[k-1].body);
-		if (k==1)
-		{
-
-			dBodySetPosition (platforma[k-1].body,0,-1.6,1);
-			dBodySetRotation (platforma[k-1].body,R2);
-			pos2 = dBodyGetPosition(platforma[k-1].body);
-			R2   = dBodyGetRotation(platforma[k-1].body);
-			dsDrawBox(pos2,R2,platforma_sides);
-		}
-		else
-		{
-		
-			dBodySetPosition (platforma[k-1].body,0,(odl[DL_Most-1]+1.6),(0.5*wys[DL_Most-1]));
-			dBodySetRotation (platforma[k-1].body,R2);
-			pos2 = dBodyGetPosition(platforma[k-1].body);
-			R2   = dBodyGetRotation(platforma[k-1].body);
-			platforma2_sides[2]=wys[DL_Most-1];
-			dsDrawBox(pos2,R2,platforma2_sides);
-		}
-	
-		
-  	for (int i=1; i<(DL_Most+1) ; i++)
-	{
-		
-		angle_deg[i-1] = angle[i-1] * M_PI / 180.0;
-		dRFromAxisAndAngle(R, 1, 0, 0, angle_deg[i-1]);
-		if (wys[i-1]<1 && angle_deg<0) angle_deg[i]=((-1)*angle_deg[i]) ;
-		dsSetColorAlpha (0.9,0.6,0,2);
-		dBodySetPosition (box[i-1].body,0,odl[i-1],wys[i-1]);
-		dBodySetRotation(box[i-1].body, R);
-		dGeomSetRotation(box[i-1].geom, R);
-		dGeomSetPosition(box[i-1].geom,0,odl[i-1],wys[i-1]);
-		pos1 = dBodyGetPosition(box[i-1].body);
-		R1   = dBodyGetRotation(box[i-1].body);
-		posG = dGeomGetPosition(box[i-1].geom);
-		RG  = dGeomGetRotation(box[i-1].geom);
-		wys[i]=wys[i-1]+(sides[1]*0.5*sin(angle_deg[i-1]))+(sides[1]*0.5*sin(angle_deg[i]));
-		odl[i]=odl[i-1]+(sides[1]*0.5*cos(angle_deg[i-1]))+(sides[1]*0.5*cos(angle_deg[i]));
-		dsDrawBox(pos1,R1,sides);
-	*/
-	
-	
-		
+*/
 }
 
 void MakeBox()
 {
-
 	for (int i=0; i<DL_Most ; i++)
 	{
 		box[i] = dCreateBox(space,sides[0],sides[1],sides[2]);
-
-		/* ***STARA WERSJA MAKEBOX***
-		dMass m; 
-		angle[i]=rand()%120-60;
-		angle[0]=0;
-		z=i*0.5;
-		dMassSetZero (&m);       
-		box[i].body = dBodyCreate (world);
-		dMassSetBox (&m,DENSITY,sides[0],sides[1],sides[2]);
-		dBodySetMass (box[i].body,&m);
-		dBodySetPosition (box[i].body,0,z,1);
-		dGeomSetBody(box[i].geom,box[i].body);
-		*/
 	}
-	
 }
 
 void MakePlatforma()
 {
-	odlp[0]=-3.1;
+	odlp[0]=-3.08;
 	odlp[1]=odl[DL_Most-1]+1.75;
 	wysp[0]=1;
 	wysp[1]=0.5*wys[DL_Most-1]+0.012;
 	platforma_sides2[2]=wys[DL_Most-1]+0.025;
-
-
+	
 	for (int k=1; k<3 ; k++)
 	{
 		if(k==1)
 		{
-			dMass mp; 
-			platforma[k-1].body = dBodyCreate (world);
-			dMassSetZero (&mp); 
-			dMassSetBox (&mp,DENSITY,platforma_sides[0],platforma_sides[1],platforma_sides[2]);
-			dBodySetMass (platforma[k-1].body,&mp);
-			dBodySetPosition (platforma[k-1].body,0,odlp[k-1],wysp[k-1]);
-			platforma[k-1].geom = dCreateBox(space,platforma_sides[0],platforma_sides[1],platforma_sides[2]);
-			dGeomSetBody(platforma[k-1].geom,platforma[k-1].body);
+			platforma[k-1] = dCreateBox(space,platforma_sides[0],platforma_sides[1],platforma_sides[2]);
 		}
 	else
 		{
-			dMass mp; 
-			platforma[k-1].body = dBodyCreate (world);
-			dMassSetZero (&mp); 
-			dMassSetBox (&mp,DENSITY,platforma_sides[0],platforma_sides[1],platforma_sides2[2]);
-			dBodySetMass (platforma[k-1].body,&mp);
-			dBodySetPosition (platforma[k-1].body,0,odlp[k-1],wysp[k-1]);
-			platforma[k-1].geom = dCreateBox(space,platforma_sides[0],platforma_sides[1],platforma_sides2[2]);
-			dGeomSetBody(platforma[k-1].geom,platforma[k-1].body);
+			platforma[k-1] = dCreateBox(space,platforma_sides2[0],platforma_sides2[1],platforma_sides2[2]);
 		}
 	}
 }
-
+ 
 void MakeBall()
 {
-
 	for (int i=0; i<Test_Pilek ; i++)
 	{
-		dMass m;
+		dMass p;
 		ball[i].body=dBodyCreate(world);
-		dMassSetZero(&m);
-		dMassSetSphereTotal(&m,m1,r);
-		dBodySetMass(ball[i].body,&m);
+		dMassSetZero(&p);
+		dMassSetSphereTotal(&p,m1,r);
+		dBodySetMass(ball[i].body,&p);
 		dBodySetPosition(ball[i].body, 0.0, (i*3), 8.0);
 		ball[i].geom = dCreateSphere(space,r);
 		dGeomSetBody(ball[i].geom,ball[i].body);
 	}
 }
 
-void WartosciPoczatkowe() // Wylosowanie kątów, ustawienie belek i platform
+void WartosciPoczatkowe() // Wylosowanie katow, ustawienie belek i platform
 {
 	angle[0]=0;
 	angle[DL_Most-1]=0;
 
+	for (int l=1;l<(Ilosc_Aut-1);l++) //Losowanie kolorow - NIE DZIALA
+	{
+		samochod[l].czerwony=(rand()%100)/100;
+		samochod[l].zielony=(rand()%100)/100;
+		samochod[l].niebieski=(rand()%100)/100;
+	}
+
 	for (int i=1;i<(DL_Most-1);i++)
 	{
-		angle[i]=rand()%120-60;
+		angle[i]=rand()%130-65;
 		angle_deg[i] = angle[i] * M_PI / 180.0;
 	}
 
@@ -273,17 +275,217 @@ void WartosciPoczatkowe() // Wylosowanie kątów, ustawienie belek i platform
 		wys[o]=1.975;
 		odl[o]=-1.375;
 	}
-			
+
 	for (int j=1;j<DL_Most;j++)
 	{
 		wys[j]=wys[j-1]+(sides[1]*0.5*sin(angle_deg[j-1]))+(sides[1]*0.5*sin(angle_deg[j]));
-		odl[j]=0.05+odl[j-1]+(sides[1]*0.5*cos(angle_deg[j-1]))+(sides[1]*0.5*cos(angle_deg[j]));
+		odl[j]=odl[j-1]+(sides[1]*0.5*cos(angle_deg[j-1]))+(sides[1]*0.5*cos(angle_deg[j]));
 	}
 }
 
+void losuj_parametry(int licznik) //Losowanie parametrow samochodow
+
+{
+	int zmienna;
+	
+	samochod[licznik].length=0.2;
+	samochod[licznik].radius=((rand()%20)+20)*0.01;
+	samochod[licznik].wysokosc=0.15;
+
+	samochod[licznik].os=2*samochod[licznik].radius+0.1;
+	samochod[licznik].WymiaryPoprz[0]=0.5;
+	samochod[licznik].WymiaryPoprz[1]=samochod[licznik].os;
+	samochod[licznik].WymiaryPoprz[2]=0.02;
+
+	samochod[licznik].WymiaryRama[0]=0.5;
+	samochod[licznik].WymiaryRama[1]=sqrt((0.25*samochod[licznik].os*samochod[licznik].os)+(samochod[licznik].wysokosc*samochod[licznik].wysokosc));
+	samochod[licznik].WymiaryRama[2]=0.02;
+	cout<<samochod[licznik].radius ;
+	cout<<samochod[licznik].wysokosc;
+	cout<<samochod[licznik].os;
+
+}
+
+void Car:: MakeRama(int licznik) //kabina samochodu
+{			
+			
+	dReal x,y,q,y2;
+	dReal kat;
+
+	kat=wysokosc/(0.5*os);
+	kat=atan(kat);
+
+	q=0-0.5-przesuniecie*licznik+((platforma_sides[0]/2)-1);
+	x=odlp[0]+0.25*os;
+	y=wysp[0]+platforma_sides[2]/2+(radius-wysokosc)-0.02;
+	y2=wysp[0]+platforma_sides[2]/2;
+
+	dMass z2; 
+		dMassSetZero (&z2);       
+		Rama[0].body = dBodyCreate (world);
+		dMassSetBox (&z2,DENSITY,WymiaryRama[0],WymiaryRama[1],WymiaryRama[2]);
+		dBodySetMass (Rama[0].body,&z2);
+		dRFromAxisAndAngle(R5,1,0,0,-kat);
+		dBodySetRotation(Rama[0].body,R5);
+		dBodySetPosition (Rama[0].body,q,x,y+0.5*wysokosc);
+		Rama[0].geom = dCreateBox(space,WymiaryRama[0],WymiaryRama[1],WymiaryRama[2]);
+		dGeomSetBody(Rama[0].geom,Rama[0].body);
+			
+	dMass z6; 
+		dMassSetZero (&z6);       
+		Rama[1].body = dBodyCreate (world);
+		dMassSetBox (&z6,DENSITY,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
+		dBodySetMass (Rama[1].body,&z6);
+		dRFromAxisAndAngle(R4,1,0,0,kat);
+		dBodySetRotation(Rama[1].body,R4);
+		dBodySetPosition (Rama[1].body,q,x+WymiaryRama[1]*sin(90-kat)-WymiaryRama[2]*sin(kat),y+0.5*wysokosc);
+		Rama[1].geom = dCreateBox(space,WymiaryRama[0],WymiaryRama[1],WymiaryRama[2]);
+		dGeomSetBody(Rama[1].geom,Rama[1].body);
+
+	joint[2] = dJointCreateFixed(world, 0);
+		dJointAttach(joint[2], Rama[1].body, Rama[0].body);
+		dJointSetFixed(joint[2]);
+
+	joint[3] = dJointCreateFixed(world, 0);
+		dJointAttach(joint[3], Rama[1].body, RamaP[0].body);
+		dJointSetFixed(joint[3]);
+
+	dMass z7; 
+		dMassSetZero (&z7);       
+		Rama[2].body = dBodyCreate (world);
+		dMassSetBox (&z7,DENSITY,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
+		dBodySetMass (Rama[2].body,&z7);
+		dRFromAxisAndAngle(R4,1,0,0,kat);
+		dBodySetRotation(Rama[2].body,R4);
+		dBodySetPosition (Rama[2].body,q,x,y2+radius+0.5*wysokosc+WymiaryPoprz[2]);
+		Rama[2].geom = dCreateBox(space,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
+		dGeomSetBody(Rama[2].geom,Rama[2].body);
+
+			
+	dMass z8;
+		dMassSetZero (&z8);      
+		Rama[3].body = dBodyCreate (world);
+		dMassSetBox (&z8,DENSITY,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
+		dBodySetMass (Rama[3].body,&z8);
+		dRFromAxisAndAngle(R5,1,0,0,-kat);
+		dBodySetRotation(Rama[3].body,R5);
+		dBodySetPosition (Rama[3].body,q,x+WymiaryRama[1]*sin(90-kat)-WymiaryRama[2]*sin(kat),y2+radius+0.5*wysokosc+WymiaryPoprz[2]);
+		Rama[3].geom = dCreateBox(space,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
+		dGeomSetBody(Rama[3].geom,Rama[3].body);
+
+	joint[4] = dJointCreateFixed(world, 0);
+        dJointAttach(joint[4], Rama[2].body, Rama[3].body);
+		dJointSetFixed(joint[4]);
+
+	joint[5] = dJointCreateFixed(world, 0);
+        dJointAttach(joint[5], Rama[3].body, RamaP[0].body);
+		dJointSetFixed(joint[5]);
+		
+}
+
+void Car::MakeCar(int licznik)
+
+{
+
+	dReal x,y,q;
+	q=0-przesuniecie*licznik+((platforma_sides[0]/2)-1);
+	x=odlp[0];
+	y=wysp[0]+platforma_sides[2]/2;
+
+	dMass m; 
+		dMassSetZero (&m);       
+		Opona[0].body = dBodyCreate (world);
+		dMassSetCylinder(&m,DENSITY,3,radius,length);
+		dBodySetMass (Opona[0].body,&m);
+		dRFromAxisAndAngle(R,0,1,0, pi/2);
+		dBodySetPosition(Opona[0].body,q-0.1,x,y+radius);
+		dBodySetRotation(Opona[0].body,R);
+		Opona[0].geom=dCreateCylinder(space,radius,length);
+		dGeomSetBody(Opona[0].geom,Opona[0].body);
+		//dBodySetTorque(Opona[0].body,-150,0,0);
+
+	dMass z; 
+		dMassSetZero (&z);       
+		Opona[1].body = dBodyCreate (world);
+		dMassSetCylinder(&z,DENSITY,3,radius,length);
+		dBodySetMass (Opona[1].body,&z);
+		dRFromAxisAndAngle(R,0,1,0, pi/2);
+		dBodySetPosition(Opona[1].body,q-0.9,x,y+radius);
+		dBodySetRotation(Opona[1].body,R);
+		Opona[1].geom=dCreateCylinder(space,radius,length);
+		dGeomSetBody(Opona[1].geom,Opona[1].body);
+		//dBodySetTorque(Opona[1].body,-150,0,0);
+	
+	joint[0] = dJointCreateFixed(world, 0);
+		dJointAttach(joint[0], Opona[0].body,Opona[1].body);
+		dJointSetFixed(joint[0]);
+
+	joint[1] = dJointCreateFixed(world, 0);
+		dJointAttach(joint[1], Opona[2].body,Opona[3].body);
+		dJointSetFixed(joint[1]);
+
+	dMass z5;
+		dMassSetZero (&z5);      
+		RamaP[0].body = dBodyCreate (world);
+		dMassSetBox (&z5,DENSITY,WymiaryPoprz[0],WymiaryPoprz[1],WymiaryPoprz[2]);
+		dBodySetMass (RamaP[0].body,&z);
+		dBodySetPosition (RamaP[0].body,q-0.5,x+(os/2),y+radius);
+		RamaP[0].geom = dCreateBox(space,WymiaryPoprz[0],WymiaryPoprz[1],WymiaryPoprz[2]);
+		dGeomSetBody(RamaP[0].geom,RamaP[0].body);
+		
+	MakeRama(licznik);
+
+	dMass z3; 
+		dMassSetZero (&z3);       
+		Opona[2].body = dBodyCreate (world);
+		dMassSetCylinder(&z3,DENSITY,3,radius,length);
+		dBodySetMass (Opona[2].body,&z3);
+		dRFromAxisAndAngle(R,0,1,0, pi/2);
+		dBodySetPosition(Opona[2].body,q-0.10,x+os,y+radius);
+		dBodySetRotation(Opona[2].body,R);
+		Opona[2].geom=dCreateCylinder(space,radius,length);
+		dGeomSetBody(Opona[2].geom,Opona[2].body);
+		//dBodySetTorque(Opona[2].body,-150,0,0);
+
+
+	dMass z4; 
+		dMassSetZero (&z4);       
+		Opona[3].body = dBodyCreate (world);
+		dMassSetCylinder(&z4,DENSITY,3,radius,length);
+		dBodySetMass (Opona[3].body,&z4);
+		dRFromAxisAndAngle(R,0,1,0, pi/2);
+		dBodySetPosition(Opona[3].body,q-0.9,x+os,y+radius);
+		dBodySetRotation(Opona[3].body,R);
+		Opona[3].geom=dCreateCylinder(space,radius,length);
+		dGeomSetBody(Opona[3].geom,Opona[3].body);
+		//dBodySetTorque(Opona[3].body,-150,0,0);
+
+	joint2[1] = dJointCreateHinge2(world, 0);
+		dJointAttach(joint2[1], Opona[0].body, RamaP[0].body);
+		dJointSetHinge2Anchor(joint2[1],q-0.1,x,y+radius);
+		dJointSetHinge2Axis1(joint2[1], 0,1,0);
+		dJointSetHinge2Axis2(joint2[1],1, 0,0);
+
+	joint2[2] = dJointCreateHinge2(world, 0);
+		dJointAttach(joint2[2], Opona[1].body, RamaP[0].body);
+		dJointSetHinge2Anchor(joint2[2],q-0.9,x,y+radius);
+		dJointSetHinge2Axis1(joint2[2], 0,1,0);
+		dJointSetHinge2Axis2(joint2[2], 1,0,0);
+
+	joint2[0] = dJointCreateHinge(world, 0);
+		dJointAttach(joint2[0], Opona[2].body, RamaP[0].body);
+		dJointSetHingeAnchor(joint2[0],q-0.1,x+os,y+radius);
+		dJointSetHingeAxis(joint2[0], 1,0,0);
+
+	joint2[3] = dJointCreateHinge(world, 0);
+		dJointAttach(joint2[3], Opona[3].body, RamaP[0].body);
+		dJointSetHingeAnchor(joint2[3],q-0.9,x+os,y+radius);
+		dJointSetHingeAxis(joint2[3], 1,0,0);
+
+}
 
 int main (int argc, char **argv)
-{
+{ 
 	time_t t;
     srand(time(&t));
 
@@ -293,21 +495,28 @@ int main (int argc, char **argv)
     fn.step    = &simLoop;
     fn.command = NULL;
     fn.stop    = NULL;
-    fn.path_to_textures = "C:/ODE/drawstuff/textures";
-		
+    fn.path_to_textures = "C:/ODE/ode-0.9/drawstuff/textures";
+	
     dInitODE();             
     world = dWorldCreate(); 
 	space = dHashSpaceCreate(0);
     contactgroup = dJointGroupCreate(0);
 
 	dWorldSetGravity(world,0,0,-2);
-
+	dWorldSetCFM(world,10E-7);
+	dWorldSetERP(world,0.2);
 	ground = dCreatePlane(space,0,0,1,0);
-	
+
 	WartosciPoczatkowe();
 	MakeBox();
 	MakePlatforma();
-	MakeBall();
+	//MakeBall();
+	
+	for (int l=0;l<Ilosc_Aut;l++)
+		{
+			losuj_parametry(l);
+			samochod[l].MakeCar(l);
+		}
 
     dsSimulationLoop (argc,argv,1200,600,&fn);
 

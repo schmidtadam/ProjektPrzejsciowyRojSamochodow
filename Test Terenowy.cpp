4ec1e81894ff83692019d2bdcb2fcb3e88d5fad9
NBlastMax <nblastmax@gmail.com>
2014-06-03 20:18:27 +0200

    Najnowsza stabilna wersja.
    Zmniejszono w niej liczbę bugów i niespodziewanych błędów.
    Dodano też wyznaczanie zwycięzcy na podstawie tego który samochodzik dojechał do mety.
    Rozszerzono działanie losowania parametrów aut.
    Dodano zapobieganie przenikania mostu przez ziemię.
    Działają kolory.

--- Test Terenowy.cpp
+++ Test Terenowy.cpp
@@ -1,4 +1,4 @@
-#include <ode/ode.h>
+ï»¿#include <ode/ode.h>
 #include <drawstuff/drawstuff.h>
 #include <time.h> 
 #include <conio.h>
@@ -24,6 +24,9 @@ static dWorldID world;
 static dSpaceID space;
 static dGeomID  ground;
 static dJointGroupID contactgroup;
+void ZniszczAuta();
+void losuj_parametry(int licznik);
+void nadaj_parametry(int zwyciezca);
 
 struct MyObject
 {
@@ -61,7 +64,7 @@ class Car
 
 Car::Car()
 {
-	const dReal m = 0.1; //masa ko³a
+	const dReal m = 0.1; //masa koï¿½a
 	const dReal z = 0.1; //masa czesci kabiny
 	const dReal z2 = 0.1; //masa czesci kabiny
 	const dReal z3 = 0.1; //masa czesci kabiny
@@ -75,7 +78,7 @@ Car::Car()
 dReal przesuniecie=2;
 
 const int Ilosc_Aut = 4;
-const int DL_Most = 30;
+const int DL_Most = 50;
 const int Test_Pilek = 5;
 
 //Obiekty
@@ -92,9 +95,14 @@ const dReal p = 0.05;
 //Macierz obrotu
 dMatrix3 R; 
 dReal pi=3.14;
+dReal a,meta;
+int zwyciezca=Ilosc_Aut;
+int czas=0;
+
+
 
 //Platformy i belki
-dReal sides[3] = {Ilosc_Aut*2.2,0.4,0.05};
+dReal sides[3] = {Ilosc_Aut*2.2,0.34,0.05};
 dReal platforma_sides[3] = {Ilosc_Aut*2.2,3,2};
 dReal platforma_sides2[3] = {Ilosc_Aut*2.2,3,0};
 
@@ -133,80 +141,135 @@ static void start()
     dsSetViewpoint (xyz,hpr);           
 }
 
-static void simLoop (int pause)
+
+void ZniszczAuta()
 {
-    const dReal *pos1,*R1,*pos2,*R2, *pos3,*R3,*pos4,*R4,*pos6,*R6,*pos7,*R7;
-	
-	dSpaceCollide(space,0,&nearCallback);
-	
-	dWorldStep(world,0.01);
-	dJointGroupEmpty(contactgroup);
-
-	for (int j=1; j<(DL_Most+1); j++) //Belki
-	{
-		dRFromAxisAndAngle(R, 1, 0, 0, angle_deg[j-1]);
-		dsSetColorAlpha (0.9,0.6,0,2);
-		dGeomSetPosition(box[j-1],0,odl[j-1],wys[j-1]);
-		dGeomSetRotation(box[j-1],R);
-		pos1 = dGeomGetPosition(box[j-1]);
-		R1 = dGeomGetRotation(box[j-1]);
-		dGeomBoxGetLengths (box[j-1],sides);
-		dsDrawBox(pos1,R1,sides);
-	}
-
-	for (int k=1; k<3; k++) //Platformy
-	{
-		dsSetColorAlpha (0.5,0.5,0.5,1);
-		dGeomSetPosition(platforma[k-1],0,odlp[k-1],wysp[k-1]);
-		pos2 = dGeomGetPosition(platforma[k-1]);
-		R2   = dGeomGetRotation(platforma[k-1]);
-		if (k==1) {	dsDrawBox(pos2,R2,platforma_sides);}
-		else dsDrawBox(pos2,R2,platforma_sides2);
-	}
-
 	for(int l=0;l<Ilosc_Aut;l++)//Auta
 	{
-		for (int i=0; i<4 ; i++) //Opona
-		{
-    		dsSetColorAlpha (1,1,1,1);
-			pos6 = dBodyGetPosition(samochod[l].Opona[i].body);
-			R6 = dBodyGetRotation(samochod[l].Opona[i].body);
-			dsDrawCylinder(pos6,R6,samochod[l].length,samochod[l].radius);
-		}
+		for (int i=0; i<5 ; i++) //Opona
+		{	
+			if (i<1) 
+			{
+				dGeomDestroy(samochod[l].RamaP[i].geom);
+			}
 
-		for (int i=0; i<1 ; i++) //Auto
-		{
-			dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
-			pos4 = dBodyGetPosition(samochod[l].RamaP[i].body);
-			R4   = dBodyGetRotation(samochod[l].RamaP[i].body);
-			dsDrawBox(pos4,R4,samochod[l].WymiaryPoprz);
-	    }
-		
-		for (int i=0; i<4 ; i++) //Auto
-		{
-			dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
-			pos7 = dBodyGetPosition(samochod[l].Rama[i].body);
-			R7   = dBodyGetRotation(samochod[l].Rama[i].body);
-			dsDrawBox(pos7,R7,samochod[l].WymiaryRama);
-		}
-
-	dBodySetAngularVel(samochod[l].Opona[0].body, -3, 0, 0);
-	dBodySetAngularVel(samochod[l].Opona[1].body, -3, 0, 0); 
-	dBodySetAngularVel(samochod[l].Opona[2].body, -3, 0, 0);
-	dBodySetAngularVel(samochod[l].Opona[3].body, -3, 0, 0);
-}
-
-	/* KULE DO TESTU FIZYKI - NIEAKTYWNE
-	for (int p=0; p<Test_Pilek; p++) // Kule
-	{
-	    dsSetColor(0.0,0.5,0.0);
-		dsSetSphereQuality(3);
-		pos3 = dBodyGetPosition(ball[p].body);
-		R3   = dBodyGetRotation(ball[p].body);
-		dsDrawSphere(pos3,R3,r);
+			if (i<4)
+			{
+				dGeomDestroy(samochod[l].Opona[i].geom);
+				dGeomDestroy(samochod[l].Rama[i].geom);
+			}
+		}	
 	}
-*/
 }
+
+
+
+
+static void simLoop (int pause)
+{
+   const dReal *pos1,*R1,*pos2,*R2, *pos3,*R3,*pos4,*R4,*pos6,*R6,*pos7,*R7;
+
+	if(czas==0)
+	{
+		a=0;
+		meta=1;
+	}
+   
+	czas++;
+
+	if (a>meta)
+	{	
+		ZniszczAuta();
+		czas=0;
+
+		for (int l=0;l<Ilosc_Aut;l++)
+		{
+			nadaj_parametry(zwyciezca);
+			//losuj_parametry(l);
+			samochod[l].MakeCar(l);
+		}
+	}
+
+dSpaceCollide(space,0,&nearCallback);
+
+dWorldStep(world,0.01);
+dJointGroupEmpty(contactgroup);
+
+for (int j=1; j<(DL_Most+1); j++) //Belki
+{
+	dRFromAxisAndAngle(R, 1, 0, 0, angle_deg[j-1]);	
+	dsSetColorAlpha (0.9,0.6,0,2);
+	dGeomSetPosition(box[j-1],0,odl[j-1],wys[j-1]);
+	dGeomSetRotation(box[j-1],R);
+	pos1 = dGeomGetPosition(box[j-1]);
+	R1 = dGeomGetRotation(box[j-1]);
+	dGeomBoxGetLengths (box[j-1],sides);
+	dsDrawBox(pos1,R1,sides);
+}
+
+for (int k=1; k<3; k++) //Platformy
+{
+	dsSetColorAlpha (0.5,0.5,0.5,1);
+	dGeomSetPosition(platforma[k-1],0,odlp[k-1],wysp[k-1]);
+	pos2 = dGeomGetPosition(platforma[k-1]);
+	R2   = dGeomGetRotation(platforma[k-1]);
+	if (k==1) dsDrawBox(pos2,R2,platforma_sides);
+	else 
+	{
+		dsDrawBox(pos2,R2,platforma_sides2);
+		meta=pos2[1];
+	}
+
+}
+
+for(int l=0;l<Ilosc_Aut;l++)//Auta
+{
+	for (int i=0; i<4 ; i++) //Opona
+	{
+   		 dsSetColorAlpha (1,1,1,1);
+		pos6 = dBodyGetPosition(samochod[l].Opona[i].body);
+		R6 = dBodyGetRotation(samochod[l].Opona[i].body);
+		dsDrawCylinder(pos6,R6,samochod[l].length,samochod[l].radius);
+	}
+
+	for (int i=0; i<1 ; i++) //Auto
+	{
+		dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
+		pos4 = dBodyGetPosition(samochod[l].RamaP[i].body);
+		R4   = dBodyGetRotation(samochod[l].RamaP[i].body);
+		dsDrawBox(pos4,R4,samochod[l].WymiaryPoprz);
+		a=max(a,pos4[1]);
+		if(a==pos4[1]) zwyciezca=l;
+	}
+
+	for (int i=0; i<4 ; i++) //Auto
+	{
+		dsSetColorAlpha (samochod[l].czerwony,samochod[l].zielony,samochod[l].niebieski,1);
+		pos7 = dBodyGetPosition(samochod[l].Rama[i].body);
+		R7   = dBodyGetRotation(samochod[l].Rama[i].body);
+		dsDrawBox(pos7,R7,samochod[l].WymiaryRama);
+	}
+
+	dBodySetAngularVel(samochod[l].Opona[0].body, -5, 0, 0);
+	dBodySetAngularVel(samochod[l].Opona[1].body, -5, 0, 0); 
+	dBodySetAngularVel(samochod[l].Opona[2].body, -5, 0, 0);
+	dBodySetAngularVel(samochod[l].Opona[3].body, -5, 0, 0);
+}
+
+/* KULE DO TESTU FIZYKI - NIEAKTYWNE
+for (int p=0; p<Test_Pilek; p++) // Kule
+{
+   dsSetColor(0.0,0.5,0.0);
+dsSetSphereQuality(3);
+pos3 = dBodyGetPosition(ball[p].body);
+R3   = dBodyGetRotation(ball[p].body);
+dsDrawSphere(pos3,R3,r);
+}
+*/
+
+}
+
+
 
 void MakeBox()
 {
@@ -230,7 +293,7 @@ void MakePlatforma()
 		{
 			platforma[k-1] = dCreateBox(space,platforma_sides[0],platforma_sides[1],platforma_sides[2]);
 		}
-	else
+		else
 		{
 			platforma[k-1] = dCreateBox(space,platforma_sides2[0],platforma_sides2[1],platforma_sides2[2]);
 		}
@@ -257,12 +320,7 @@ void WartosciPoczatkowe() // Wylosowanie katow, ustawienie belek i platform
 	angle[0]=0;
 	angle[DL_Most-1]=0;
 
-	for (int l=1;l<(Ilosc_Aut-1);l++) //Losowanie kolorow - NIE DZIALA
-	{
-		samochod[l].czerwony=(rand()%100)/100;
-		samochod[l].zielony=(rand()%100)/100;
-		samochod[l].niebieski=(rand()%100)/100;
-	}
+	
 
 	for (int i=1;i<(DL_Most-1);i++)
 	{
@@ -278,8 +336,18 @@ void WartosciPoczatkowe() // Wylosowanie katow, ustawienie belek i platform
 
 	for (int j=1;j<DL_Most;j++)
 	{
-		wys[j]=wys[j-1]+(sides[1]*0.5*sin(angle_deg[j-1]))+(sides[1]*0.5*sin(angle_deg[j]));
-		odl[j]=odl[j-1]+(sides[1]*0.5*cos(angle_deg[j-1]))+(sides[1]*0.5*cos(angle_deg[j]));
+		if (wys[j-1]<1)
+		{
+			if (angle[j]<0) 
+			angle_deg[j]=0-(angle[j] * M_PI / 180.0);
+			wys[j]=wys[j-1]+(sides[1]*0.5*sin(angle_deg[j-1]))+(sides[1]*0.5*sin(angle_deg[j]));
+			odl[j]=odl[j-1]+(sides[1]*0.5*cos(angle_deg[j-1]))+(sides[1]*0.5*cos(angle_deg[j]));
+		}
+		else
+		{
+			wys[j]=wys[j-1]+(sides[1]*0.5*sin(angle_deg[j-1]))+(sides[1]*0.5*sin(angle_deg[j]));
+			odl[j]=odl[j-1]+(sides[1]*0.5*cos(angle_deg[j-1]))+(sides[1]*0.5*cos(angle_deg[j]));
+		}
 	}
 }
 
@@ -289,10 +357,15 @@ void losuj_parametry(int licznik) //Losowanie parametrow samochodow
 	int zmienna;
 	
 	samochod[licznik].length=0.2;
-	samochod[licznik].radius=((rand()%20)+20)*0.01;
-	samochod[licznik].wysokosc=0.15;
-
-	samochod[licznik].os=2*samochod[licznik].radius+0.1;
+	//samochod[licznik].radius=((rand()%30)+15)*0.01;
+	
+	samochod[licznik].wysokosc=((rand()%15)+15)*0.01;
+	zmienna=samochod[licznik].wysokosc*100+7;
+	samochod[licznik].radius=((rand()%30)+zmienna)*0.01;
+	//samochod[licznik].radius=0.4;
+	zmienna=(2*samochod[licznik].radius+0.1)*100;
+	samochod[licznik].os=((rand()%10)+zmienna)*0.01;
+		
 	samochod[licznik].WymiaryPoprz[0]=0.5;
 	samochod[licznik].WymiaryPoprz[1]=samochod[licznik].os;
 	samochod[licznik].WymiaryPoprz[2]=0.02;
@@ -304,6 +377,40 @@ void losuj_parametry(int licznik) //Losowanie parametrow samochodow
 	cout<<samochod[licznik].wysokosc;
 	cout<<samochod[licznik].os;
 
+	samochod[licznik].czerwony=((rand()%100)+0)*0.01;
+	samochod[licznik].zielony=((rand()%100)+0)*0.01;
+	samochod[licznik].niebieski=((rand()%100)+0)*0.01;
+
+}
+
+void nadaj_parametry(int zwyciezca) //Losowanie parametrow samochodow
+
+{
+	for(int licznik=0;licznik<Ilosc_Aut;licznik++)
+	{
+	
+	samochod[licznik].length=samochod[zwyciezca].length;
+	//samochod[licznik].radius=((rand()%30)+15)*0.01;
+	
+	samochod[licznik].wysokosc=samochod[zwyciezca].wysokosc;
+	
+	samochod[licznik].radius=samochod[zwyciezca].radius;
+	//samochod[licznik].radius=0.4;
+	
+	samochod[licznik].os=samochod[zwyciezca].os;
+		
+	samochod[licznik].WymiaryPoprz[0]=0.5;
+	samochod[licznik].WymiaryPoprz[1]=samochod[zwyciezca].WymiaryPoprz[1];
+	samochod[licznik].WymiaryPoprz[2]=0.02;
+
+	samochod[licznik].WymiaryRama[0]=0.5;
+	samochod[licznik].WymiaryRama[1]=samochod[zwyciezca].WymiaryRama[1];
+	samochod[licznik].WymiaryRama[2]=0.02;
+
+	samochod[licznik].czerwony=samochod[zwyciezca].czerwony;
+	samochod[licznik].zielony=samochod[zwyciezca].zielony;
+	samochod[licznik].niebieski=samochod[zwyciezca].niebieski;
+	}
 }
 
 void Car:: MakeRama(int licznik) //kabina samochodu
@@ -338,7 +445,7 @@ void Car:: MakeRama(int licznik) //kabina samochodu
 		dBodySetMass (Rama[1].body,&z6);
 		dRFromAxisAndAngle(R4,1,0,0,kat);
 		dBodySetRotation(Rama[1].body,R4);
-		dBodySetPosition (Rama[1].body,q,x+WymiaryRama[1]*sin(90-kat)-WymiaryRama[2]*sin(kat),y+0.5*wysokosc);
+		dBodySetPosition (Rama[1].body,q,x+WymiaryRama[1]*cos(kat)+WymiaryRama[2],y+0.5*wysokosc);
 		Rama[1].geom = dCreateBox(space,WymiaryRama[0],WymiaryRama[1],WymiaryRama[2]);
 		dGeomSetBody(Rama[1].geom,Rama[1].body);
 
@@ -369,7 +476,7 @@ void Car:: MakeRama(int licznik) //kabina samochodu
 		dBodySetMass (Rama[3].body,&z8);
 		dRFromAxisAndAngle(R5,1,0,0,-kat);
 		dBodySetRotation(Rama[3].body,R5);
-		dBodySetPosition (Rama[3].body,q,x+WymiaryRama[1]*sin(90-kat)-WymiaryRama[2]*sin(kat),y2+radius+0.5*wysokosc+WymiaryPoprz[2]);
+		dBodySetPosition (Rama[3].body,q,x+WymiaryRama[1]*cos(kat)+WymiaryRama[2],y2+radius+0.5*wysokosc+WymiaryPoprz[2]);
 		Rama[3].geom = dCreateBox(space,WymiaryPoprz[0],WymiaryRama[1],WymiaryPoprz[2]);
 		dGeomSetBody(Rama[3].geom,Rama[3].body);
 
@@ -486,21 +593,23 @@ void Car::MakeCar(int licznik)
 
 int main (int argc, char **argv)
 { 
-	time_t t;
-    srand(time(&t));
 
-    dsFunctions fn;
-    fn.version = DS_VERSION;
-    fn.start   = &start;
-    fn.step    = &simLoop;
-    fn.command = NULL;
-    fn.stop    = NULL;
-    fn.path_to_textures = "C:/ODE/ode-0.9/drawstuff/textures";
-	
-    dInitODE();             
-    world = dWorldCreate(); 
-	space = dHashSpaceCreate(0);
-    contactgroup = dJointGroupCreate(0);
+time_t t;
+   srand(time(&t));
+
+   dsFunctions fn;
+   fn.version = DS_VERSION;
+   fn.start   = &start;
+   fn.step    = &simLoop;
+   fn.command = NULL;
+   fn.stop    = NULL;
+   fn.path_to_textures = "C:/ODE/ode-0.9/drawstuff/textures";
+
+   dInitODE();
+
+   world = dWorldCreate(); 
+   space = dHashSpaceCreate(0);
+   contactgroup = dJointGroupCreate(0);
 
 	dWorldSetGravity(world,0,0,-2);
 	dWorldSetCFM(world,10E-7);
@@ -510,18 +619,19 @@ int main (int argc, char **argv)
 	WartosciPoczatkowe();
 	MakeBox();
 	MakePlatforma();
-	//MakeBall();
-	
+
 	for (int l=0;l<Ilosc_Aut;l++)
-		{
-			losuj_parametry(l);
-			samochod[l].MakeCar(l);
-		}
+	{
+		losuj_parametry(l);
+		samochod[l].MakeCar(l);
+	}
 
-    dsSimulationLoop (argc,argv,1200,600,&fn);
+	//MakeBall();
 
-    dWorldDestroy (world); 
-    dCloseODE();           
+	dsSimulationLoop (argc,argv,1200,600,&fn);
+	dWorldDestroy (world); 
 
-    return 0;
-}
+	dCloseODE();           
+
+   return 0;
+}           
